e8a69be6678bb6f6d65ec511ca3a1c5b1c78d735
gabrielronny <gabrielronny123@hotmail.com>
2023-09-28 18:43:42 -0300

    feat: initial page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import './Reset.css';
+import Menu from './components/menu/Menu';
 
 function App() {
   return (
     <div className="App">
+      {/* <Menu /> */}
+      <div className="container">
+        <div className="content">
+          <span>Olá, estamos construindo o futuro para seu negócio</span>
+        </div>
+      </div>
     </div>
   );
 }
