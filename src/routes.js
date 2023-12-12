ebfa9082eaf62142f7759b2f41f50b51d85bf715
gabrielronny <gabrielronny123@hotmail.com>
2023-12-12 01:25:01 -0300

    fix: ajuste na chamada da página 404

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 
 // Pages
 import Home from "./Pages/Home/Home";
-import NotFoundPage from "./Pages/NotFound/NotFoundPage";
+// import NotFoundPage from "./Pages/NotFound/NotFoundPage";
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" Component={Home} />
-        <Route path="*" component={NotFoundPage} />
+        {/* <Route path="*" component={NotFoundPage} /> */}
       </Routes>
     </BrowserRouter>
   );
